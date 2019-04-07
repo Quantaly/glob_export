53454158d756bb9454815366ef521cbf838d5906
Kai Page <kaibug@gmail.com>
2019-04-07 16:30:44 -0500

    Init commit

---
+++ example/glob_export_example.dart
@@ -0,0 +1,6 @@
+import 'package:glob_export/glob_export.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/glob_export.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library glob_export;
+
+export 'src/glob_export_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
