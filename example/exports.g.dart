fd9a94839c491128607db1dbd753fa1b5830cbda
Kai Page <kaibug@gmail.com>
2019-04-07 21:57:44 -0500

    It... kinda works?
    Just don't look too closely

---
+++ example/exports.g.dart
@@ -0,0 +1,4 @@
+export 'a_file.dart';
+export 'b_file.dart';
+export 'morefiles/d_file.dart';
+export 'morefiles/e_fiel.dart';
--- lib/src/builder.dart
+++ lib/src/builder.dart
@@ -4,8 +4,10 @@ import 'package:build/build.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:glob/glob.dart';
+import 'package:path/path.dart' as path;
 
-final _newline = RegExp("\\r?\\n?");
+final _newline = RegExp("\\r\\n|\\r|\\n");
+final _path = path.Context(style: path.Style.url);
 
 /// The main [Builder] used by this package.
 class GlobExportBuilder extends Builder {
@@ -17,16 +19,17 @@ class GlobExportBuilder extends Builder {
   @override
   Future<void> build(BuildStep buildStep) async {
     var fileContents = await buildStep.readAsString(buildStep.inputId);
+    //var allAssets = await buildStep.findAssets(Glob("**")).toSet();
     var exportIds = Set<AssetId>();
     for (var line in fileContents.split(_newline)) {
-      exportIds.addAll(await buildStep.findAssets(Glob(line)).toSet());
+      String glob = _path.joinAll(List.from(buildStep.inputId.pathSegments)..removeLast()..add(line));
+      print("the glob is $glob");
+      exportIds.addAll(await buildStep
+          .findAssets(Glob(glob))
+          .toSet());
     }
 
-    var exports = Set<Directive>();
-    for (var id in exportIds) {
-      exports.add(Directive.export(id.uri.toString()));
-    }
-
+    var exports = exportIds.map((id) => Directive.export(_exportUri(id.uri, buildStep.inputId.uri)));
     var library = Library((b) => b..directives.addAll(exports));
 
     var outputId = buildStep.inputId.changeExtension(".g.dart");
@@ -36,4 +39,11 @@ class GlobExportBuilder extends Builder {
         DartFormatter(fixes: StyleFix.all)
             .format(emitter.visitLibrary(library).toString()));
   }
+}
+
+String _exportUri(Uri uri, Uri baseUri) {
+  if (uri.isScheme("package")) return uri.toString();
+  var ret = _path.relative(uri.toString(), from: _path.dirname(baseUri.toString()));
+  print(ret);
+  return ret;
 }
